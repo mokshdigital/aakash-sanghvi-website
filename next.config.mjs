cab2e523ed8cc35997fc93d70b49067044e2eb77
mokshdigital <aakash@mokshdigital.com>
2026-01-03 07:50:47 -0800

    chore: Switch next.config.ts to next.config.mjs & cleanup

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Ensure trailing slashes are handled consistently if that's an issue (optional)
+    // trailingSlash: false, 
+};
+
+export default nextConfig;
